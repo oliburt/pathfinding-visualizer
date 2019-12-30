5ee341e460aa277a1d3512deedec2ff54d93c219
oliburt <oliburt@gmail.com>
2019-12-30 12:26:48 +0000

    initial commit

---
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Node from './Node/Node'
+
+import './PathfindingVisualizer.css';
+
+export default class PathfindingVisulaizer extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        Foo
+        <Node></Node>
+      </div>
+    );
+  }
+}
+
