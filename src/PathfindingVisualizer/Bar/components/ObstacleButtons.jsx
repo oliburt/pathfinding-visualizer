acaf4973c4eecc8649fdf40102f28f6cb3a8ac8c
oliburt <oliburt@gmail.com>
2020-01-03 16:17:39 +0000

    obstacle buttons

---
+++ src/PathfindingVisualizer/Bar/components/ObstacleButtons.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function ObstacleButtons({isSearchRunning}) {
+  return (
+    <div className="obstacleButtons">
+      <h5 className="title is-6">Obstacles</h5>
+
+      <button
+        className="button is-success is-small is-rounded"
+        onClick={() => {}}
+        disabled={isSearchRunning}
+      >
+        Random Walls
+      </button>
+      <br />
+      {/* <button
+        className="button is-success is-small is-rounded"
+        onClick={() => {}}
+        disabled={isSearchRunning}
+      >
+        Reset Search
+      </button> */}
+    </div>
+  );
+}
